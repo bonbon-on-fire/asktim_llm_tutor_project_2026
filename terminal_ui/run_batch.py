a37011bbb08d1001ec587e8c3fc17b6c2cb7bc3c
bonbon-on-fire <nishita@bhakars.com>
2026-03-10 01:24:28 -0400

    feat: ran chaotic simulations

--- terminal_ui/run_batch.py
+++ terminal_ui/run_batch.py
@@ -39,7 +39,7 @@ _TUTOR_GREETING = "Hi. What would you like to work on today?"
 TUTOR_PROMPTS: list[str] = ["tutor_01"]
 
 # Which student personas to run (from students/personas/*.txt, without extension).
-STUDENT_PERSONAS: list[str] = ["chaotic_01", "chaotic_02", "chaotic_03"]
+STUDENT_PERSONAS: list[str] = ["chitchat_01","chitchat_02","chitchat_03"]
 
 # Which course/exercise combinations to run.
 # Exercise numbers should be zero-padded strings like "01".
