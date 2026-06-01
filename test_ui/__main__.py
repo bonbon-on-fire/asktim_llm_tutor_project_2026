bb39f4f967a87b2182a577ff816d2bd7f0b5395e
bonbon-on-fire <nishita@bhakars.com>
2026-06-01 14:35:02 -0700

    chore: removed old files

--- test_ui/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Allow ``python -m test_ui`` to launch the Flask dev server."""
-
-from .run_app import app
-
-if __name__ == "__main__":
-    import os
-
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
